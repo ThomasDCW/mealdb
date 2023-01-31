97a875028e455b531eacdb8c4a4aee30bb399413
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-01-31 16:32:52 +0100

    mealdb fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
+      .then(({ data }) => {
+        setMeals(data);
+      });
+  }, [console.log('fetch')]);
+
+  let mealsArray = meals.meals;
+
+  console.table(mealsArray);
+
   return (
     <div className='App'>
       <header className='header'>
         <h1>MEAL DB</h1>
       </header>
+      <div>
+        {mealsArray.map((meal) => {
+          return (
+            <div>
+              <p>{meal.strMeal}</p>
+              <img src={meal.strMealThumb} alt='' className='meal-picture' />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
